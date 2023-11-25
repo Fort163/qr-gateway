47fb9a725d75a6d2e3947b7e9173f1f9b14ad227
Fort163 <Front-91@mail.ru>
2023-11-25 14:06:39 +0400

    fix company

--- src/main/java/com/quick/recording/gateway/service/company/CompanyController.java
+++ src/main/java/com/quick/recording/gateway/service/company/CompanyController.java
@@ -1,8 +1,6 @@
 package com.quick.recording.gateway.service.company;
 
 import com.quick.recording.gateway.dto.company.CompanyDto;
-import com.quick.recording.resource.service.anatation.CurrentUser;
-import com.quick.recording.resource.service.security.QROAuth2AuthenticatedPrincipal;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,10 +13,10 @@ public interface CompanyController {
 
     @GetMapping("/")
     @PreAuthorize("hasAnyAuthority('ROLE_WRITE')")
-    ResponseEntity<CompanyDto> getCompany(@CurrentUser QROAuth2AuthenticatedPrincipal user);
+    ResponseEntity<CompanyDto> getCompany();
 
     @GetMapping("/list")
     @PreAuthorize("hasAnyAuthority('ROLE_READ')")
-    ResponseEntity<List<CompanyDto>> getCompanyList(@CurrentUser QROAuth2AuthenticatedPrincipal user);
+    ResponseEntity<List<CompanyDto>> getCompanyList();
 
 }
