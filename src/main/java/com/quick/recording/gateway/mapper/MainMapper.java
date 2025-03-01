ac043b9fac9cdaca3798322f171f002b60cdaf7e
Fort163 <Front-91@mail.ru>
2025-03-01 13:16:48 +0400

    add main mapper

---
+++ src/main/java/com/quick/recording/gateway/mapper/MainMapper.java
@@ -0,0 +1,24 @@
+package com.quick.recording.gateway.mapper;
+
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.util.List;
+
+public interface MainMapper <Entity, Dto> {
+
+    Entity toEntity(Dto dto);
+
+    Dto toDto(Entity entity);
+
+    List<Dto> toDtoList(List<Entity> list);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Entity toEntityWithoutNull(Dto dto, @MappingTarget Entity entity);
+
+
+    Entity toEntity(Dto dto, @MappingTarget Entity entity);
+
+}
