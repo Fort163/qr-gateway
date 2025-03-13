667205d36fc6a579b992e8e578cbce7871ed17b1
Fort163 <Front-91@mail.ru>
2025-03-14 02:24:36 +0400

    change api service for new api interface redirect api to gateway

--- src/main/java/com/quick/recording/gateway/service/company/CompanyServiceActivityApi.java
+++ src/main/java/com/quick/recording/gateway/service/company/CompanyServiceActivityApi.java
@@ -1,39 +1,14 @@
 package com.quick.recording.gateway.service.company;
 
 import com.quick.recording.gateway.dto.company.ActivityDto;
-import com.quick.recording.gateway.dto.company.SearchActivityDto;
+import com.quick.recording.gateway.main.controller.MainController;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.SpringQueryMap;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @FeignClient(
-        name = "COMPANY-SERVICE",
+        name = "GATEWAY-SERVICE",
         contextId = "activity",
-        path = "/api/v1/activity"
+        path = "/company/api/v1/activity"
 )
-public interface CompanyServiceActivityApi {
-
-    @GetMapping({"/{uuid}"})
-    ResponseEntity<ActivityDto> byUuid(@PathVariable UUID uuid);
-
-    @GetMapping
-    Page<ActivityDto> list(@SpringQueryMap SearchActivityDto searchActivityDto, @SpringQueryMap Pageable pageable);
-
-    @PostMapping
-    ResponseEntity<ActivityDto> post(@RequestBody ActivityDto activityDto);
-
-    @PutMapping({"/patch"})
-    ResponseEntity<ActivityDto> patch(@RequestBody ActivityDto user);
-
-    @PutMapping
-    ResponseEntity<ActivityDto> put(@RequestBody ActivityDto user);
-
-    @DeleteMapping({"/{uuid}"})
-    ResponseEntity<Boolean> delete(@PathVariable UUID uuid);
+public interface CompanyServiceActivityApi extends MainController<ActivityDto> {
 
 }
--- src/main/java/com/quick/recording/gateway/service/company/CompanyServiceCompanyApi.java
+++ src/main/java/com/quick/recording/gateway/service/company/CompanyServiceCompanyApi.java
@@ -1,7 +1,6 @@
 package com.quick.recording.gateway.service.company;
 
 import com.quick.recording.gateway.dto.company.CompanyDto;
-import com.quick.recording.gateway.dto.company.SearchCompanyDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.data.domain.Page;
@@ -11,14 +10,17 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@FeignClient(name = "COMPANY-SERVICE", contextId = "company", path = "/api/v1/company")
+@FeignClient(
+        name = "COMPANY-SERVICE",
+        contextId = "company",
+        path = "/api/v1/company")
 public interface CompanyServiceCompanyApi {
 
     @GetMapping({"/{uuid}"})
     ResponseEntity<CompanyDto> byUuid(@PathVariable UUID uuid);
 
     @GetMapping
-    Page<CompanyDto> list(@SpringQueryMap SearchCompanyDto searchCompanyDto, @SpringQueryMap Pageable pageable);
+    Page<CompanyDto> list(@RequestBody CompanyDto companyDto, @SpringQueryMap Pageable pageable);
 
     @PostMapping
     ResponseEntity<CompanyDto> post(@RequestBody CompanyDto companyDto);
