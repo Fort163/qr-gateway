72f4f24da4b1127ae9835bd62f197ca004c44605
Fort163 <Front-91@mail.ru>
2025-03-14 02:15:33 +0400

    add new exception handler

--- src/main/java/com/quick/recording/gateway/config/error/QRExceptionHandler.java
+++ src/main/java/com/quick/recording/gateway/config/error/QRExceptionHandler.java
@@ -14,13 +14,17 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -46,7 +50,7 @@ public class QRExceptionHandler extends ResponseEntityExceptionHandler {
         List<String> errors = new ArrayList<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
-            String errorMessage = messageUtil.create(error.getDefaultMessage());
+            String errorMessage = error.getDefaultMessage();
             errors.add(messageUtil.create("error.validation.field", fieldName, errorMessage));
         });
         String messageError = messageUtil.create("error.validation.class",
@@ -56,6 +60,62 @@ public class QRExceptionHandler extends ResponseEntityExceptionHandler {
         );
         ApiError build = ApiError.builder().message(messageError).errors(errors).service(serviceName).build();
         return new ResponseEntity(build, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+                                                                          HttpHeaders headers,
+                                                                          HttpStatusCode status,
+                                                                          WebRequest request) {
+        ApiError build = ApiError.builder()
+                .debugMessage(ex.getMessage())
+                .message(
+                        messageUtil.create("exception.missing.parameter.request",
+                                ex.getMethodParameter().getParameter().getName(),
+                                ex.getMethodParameter().getParameter().getType().getName(),
+                                ex.getMethodParameter().getExecutable().toString()
+                                )
+                )
+                .service(serviceName)
+                .build();
+        return new ResponseEntity(build, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex,
+                                                               HttpHeaders headers,
+                                                               HttpStatusCode status,
+                                                               WebRequest request) {
+        ApiError build = ApiError.builder()
+                .debugMessage(ex.getMessage())
+                .message(
+                        messageUtil.create("exception.missing.parameter.path",
+                                ex.getParameter().getParameter().getName(),
+                                ex.getParameter().getParameter().getType().getName(),
+                                ex.getParameter().getExecutable().toString()
+                        )
+                )
+                .service(serviceName)
+                .build();
+        return new ResponseEntity(build, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoResourceFoundException(NoResourceFoundException ex,
+                                                                    HttpHeaders headers,
+                                                                    HttpStatusCode status,
+                                                                    WebRequest request) {
+        ApiError build = ApiError.builder()
+                .debugMessage(ex.getMessage())
+                .message(
+                        messageUtil.create("exception.not.found.resource",
+                                ex.getResourcePath(),
+                                ex.getHttpMethod().name()
+                        )
+                )
+                .service(serviceName)
+                .build();
+        return new ResponseEntity(build, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(FeignException.class)
