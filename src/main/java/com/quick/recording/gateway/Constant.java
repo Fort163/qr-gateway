3f1baa9e0d3245bd1b7447868df52cf229d827f3
Fort163 <Front-91@mail.ru>
2025-03-18 23:18:04 +0400

    add constant

--- src/main/java/com/quick/recording/gateway/Constant.java
+++ src/main/java/com/quick/recording/gateway/Constant.java
@@ -3,5 +3,6 @@ package com.quick.recording.gateway;
 public class Constant {
 
     public static final String LOCALE_COOKIE_NAME = "i18next";
+    public static final String TRACK_ID_HEADER_NAME = "trackId";
 
 }
