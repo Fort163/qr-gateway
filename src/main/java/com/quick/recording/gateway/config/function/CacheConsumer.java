af9d08c15df8d0779de8b5714056d3955d3fe02e
Fort163 <Front-91@mail.ru>
2025-10-06 17:23:38 +0400

    change CacheConsumer support multi functions

--- src/main/java/com/quick/recording/gateway/config/function/CacheConsumer.java
+++ src/main/java/com/quick/recording/gateway/config/function/CacheConsumer.java
@@ -1,21 +1,24 @@
 package com.quick.recording.gateway.config.function;
 
 import com.quick.recording.gateway.dto.broker.MessageChangeDataDto;
-import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
 
-@Setter
 public class CacheConsumer implements Consumer<MessageChangeDataDto> {
 
-    private RemoteCacheFunction function;
+    private List<RemoteCacheFunction> functions = new ArrayList<>();
 
     @Override
     public void accept(MessageChangeDataDto dto) {
-        if(Objects.nonNull(function)){
-            function.run(dto);
+        if (Objects.nonNull(functions) && !functions.isEmpty()) {
+            functions.forEach(function -> function.run(dto));
         }
     }
 
+    public void addFunction(RemoteCacheFunction function) {
+        this.functions.add(function);
+    }
 }
