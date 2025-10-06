581cb99636dd450a9dab724a243a2688747fba79
Fort163 <Front-91@mail.ru>
2025-10-06 17:39:56 +0400

    fix MainRemoteService

--- src/main/java/com/quick/recording/gateway/main/service/remote/MainRemoteService.java
+++ src/main/java/com/quick/recording/gateway/main/service/remote/MainRemoteService.java
@@ -2,23 +2,8 @@ package com.quick.recording.gateway.main.service.remote;
 
 import com.quick.recording.gateway.dto.BaseDto;
 import com.quick.recording.gateway.main.controller.MainController;
-import org.springframework.data.redis.core.RedisHash;
-import org.springframework.lang.Nullable;
-
-import java.util.Locale;
 
 public interface MainRemoteService<Dto extends BaseDto> extends MainController<Dto> {
-
-    Class<Dto> getType();
-
-    @Nullable
-    default String cacheName(){
-        boolean supportCache = getType().isAnnotationPresent(RedisHash.class);
-        if(supportCache){
-            return getType().getAnnotation(RedisHash.class).value().toLowerCase(Locale.ROOT);
-        }
-        return null;
-    }
 
 
 }
