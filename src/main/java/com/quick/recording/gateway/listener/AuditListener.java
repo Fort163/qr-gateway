3ffa255d57b96f6d7ae9bca6d193b9e9879bba15
Fort163 <Front-91@mail.ru>
2025-10-06 17:12:38 +0400

    fix bug in audit listener

--- src/main/java/com/quick/recording/gateway/listener/AuditListener.java
+++ src/main/java/com/quick/recording/gateway/listener/AuditListener.java
@@ -3,29 +3,42 @@ package com.quick.recording.gateway.listener;
 import com.quick.recording.gateway.entity.AuditEntity;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Component
 public class AuditListener {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     @PrePersist
     private void prePersist(AuditEntity entity) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = appName;
+        if (Objects.nonNull(authentication)) {
+            userName = authentication.getName();
+        }
         entity.setCreatedWhen(LocalDateTime.now());
         entity.setUpdatedWhen(LocalDateTime.now());
-        entity.setCreatedBy(authentication.getName());
-        entity.setUpdatedBy(authentication.getName());
+        entity.setCreatedBy(userName);
+        entity.setUpdatedBy(userName);
     }
 
     @PreUpdate
     private void preUpdate(AuditEntity entity) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = appName;
+        if (Objects.nonNull(authentication)) {
+            userName = authentication.getName();
+        }
         entity.setUpdatedWhen(LocalDateTime.now());
-        entity.setUpdatedBy(authentication.getName());
+        entity.setUpdatedBy(userName);
     }
 
 }
