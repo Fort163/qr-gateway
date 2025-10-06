9fc0e2333cf92804dcc3d58151e5b68dad448bc6
Fort163 <Front-91@mail.ru>
2025-10-06 17:13:44 +0400

    add named to main mapper

--- src/main/java/com/quick/recording/gateway/mapper/MainMapper.java
+++ src/main/java/com/quick/recording/gateway/mapper/MainMapper.java
@@ -1,25 +1,28 @@
 package com.quick.recording.gateway.mapper;
 
 
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 import java.util.List;
 
 public interface MainMapper<Entity, Dto> {
 
+    @Named(value = "toEntityDefault")
     Entity toEntity(Dto dto);
 
+    @Named(value = "toDtoDefault")
     Dto toDto(Entity entity);
 
     List<Dto> toDtoList(List<Entity> list);
 
+    @IterableMapping(qualifiedByName = "toEntityDefault")
+    List<Entity> toEntityList(List<Dto> list);
+
+    @Named(value = "toEntityWithoutNull")
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     Entity toEntityWithoutNull(Dto dto, @MappingTarget Entity entity);
 
-
+    @Named(value = "toEntityWithNull")
     Entity toEntity(Dto dto, @MappingTarget Entity entity);
 
 }
