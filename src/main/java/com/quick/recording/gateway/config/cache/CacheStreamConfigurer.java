02123dc72624c81aed712c3c8375247c536c037e
Fort163 <Front-91@mail.ru>
2025-10-06 17:34:40 +0400

    add support cache for all Controller extends MainController

--- src/main/java/com/quick/recording/gateway/config/cache/CacheStreamConfigurer.java
+++ src/main/java/com/quick/recording/gateway/config/cache/CacheStreamConfigurer.java
@@ -1,64 +1,100 @@
 package com.quick.recording.gateway.config.cache;
 
 import com.quick.recording.gateway.config.function.CacheConsumer;
+import com.quick.recording.gateway.main.controller.CacheableMainControllerAbstract;
+import com.quick.recording.gateway.main.controller.MainController;
 import com.quick.recording.gateway.main.service.remote.CacheableMainRemoteServiceAbstract;
-import com.quick.recording.gateway.main.service.remote.MainRemoteService;
-import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.MutablePropertyValues;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.type.classreading.MetadataReader;
-import org.springframework.core.type.classreading.MetadataReaderFactory;
-import org.springframework.core.type.filter.AbstractTypeHierarchyTraversingFilter;
-import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.type.filter.AssignableTypeFilter;
-import org.springframework.data.repository.CrudRepository;
 
-import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.util.*;
 
+@Log4j2
 @Configuration
 @ConditionalOnProperty(value = "qr-cache.enabled", havingValue = "true")
-public class CacheStreamConfigurer implements BeanDefinitionRegistryPostProcessor {
+public class CacheStreamConfigurer implements BeanDefinitionRegistryPostProcessor,
+        ApplicationContextInitializer<ConfigurableApplicationContext> {
 
+    private final Set<String> cacheNames = new HashSet<>();
 
     @Override
-    @SuppressWarnings("unchecked")
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-        ClassPathScanningCandidateComponentProvider scanner =
-                new ClassPathScanningCandidateComponentProvider(false);
-        scanner.addIncludeFilter(new AssignableTypeFilter(CacheableMainRemoteServiceAbstract.class));
-        Set<BeanDefinition> candidateComponents = scanner.findCandidateComponents("com/quick/recording");
-        for(BeanDefinition beanDefinition : candidateComponents){
-            try {
-                Class<? extends CacheableMainRemoteServiceAbstract<?>> clazz =
-                        (Class<? extends CacheableMainRemoteServiceAbstract<?>>)
-                                Class.forName(beanDefinition.getBeanClassName());
-                Constructor<?> constructor = Arrays.stream(clazz.getConstructors())
-                        .filter(item -> item.getParameterCount() == 2)
-                        .findFirst()
-                        .orElseThrow();
-                Object object = constructor.newInstance(null,null);
-                String cacheName = (String)clazz.getMethod("cacheName").invoke(object);
-                BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(CacheConsumer.class);
-                registry.registerBeanDefinition(cacheName, builder.getBeanDefinition());
-            }
-            catch (Exception ignored){
-            }
+        initCache();
+        for (String cacheName : this.cacheNames) {
+            BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(CacheConsumer.class);
+            registry.registerBeanDefinition(cacheName, builder.getBeanDefinition());
         }
     }
+
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         BeanDefinitionRegistryPostProcessor.super.postProcessBeanFactory(beanFactory);
     }
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        initCache();
+        try {
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            MutablePropertySources propertySources = environment.getPropertySources();
+            Map<String, Object> newProperty = new HashMap<>();
+            newProperty.put("spring.cloud.function.definition", String.join(";", this.cacheNames));
+            for (String cacheName : this.cacheNames) {
+                newProperty.put(this.propertyName(cacheName), this.destinationName(cacheName));
+            }
+            propertySources.addFirst(new MapPropertySource("Cache stream property", newProperty));
+        } catch (Exception exception) {
+            log.error("Cannot update environment " + exception.getMessage());
+        }
+    }
+
+    private String propertyName(String cacheName) {
+        return String.format("spring.cloud.stream.bindings.%s-out-0.destination", cacheName);
+    }
+
+    private String destinationName(String cacheName) {
+        return String.format("%s-in-0", cacheName);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initCache() {
+        ClassPathScanningCandidateComponentProvider scanner =
+                new ClassPathScanningCandidateComponentProvider(false);
+        scanner.addIncludeFilter(new AssignableTypeFilter(CacheableMainRemoteServiceAbstract.class));
+        scanner.addIncludeFilter(new AssignableTypeFilter(CacheableMainControllerAbstract.class));
+        Set<BeanDefinition> candidateComponents = scanner.findCandidateComponents("com/quick/recording");
+        for (BeanDefinition beanDefinition : candidateComponents) {
+            try {
+                Class<? extends MainController<?>> clazz =
+                        (Class<? extends MainController<?>>)
+                                Class.forName(beanDefinition.getBeanClassName());
+                Constructor<?> constructor = Arrays.stream(clazz.getConstructors())
+                        .filter(item -> item.getParameterCount() == 2)
+                        .findFirst()
+                        .orElseThrow();
+                Object object = constructor.newInstance(null, null);
+                Collection<String> cacheNames = (Collection<String>) clazz.getMethod("listAllCacheName").invoke(object);
+                this.cacheNames.addAll(cacheNames);
+            } catch (Exception exception) {
+                log.error("Cannot create consumer bean " + exception.getMessage());
+            }
+        }
+    }
+
 }
