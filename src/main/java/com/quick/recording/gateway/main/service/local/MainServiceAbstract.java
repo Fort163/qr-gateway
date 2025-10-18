1dfb51096bc430522c5afd3a42a358230779d09a
Fort163 <Front-91@mail.ru>
2025-10-18 12:19:07 +0400

    fix patch method rename method search to list, make method delete return dto value

--- src/main/java/com/quick/recording/gateway/main/service/local/MainServiceAbstract.java
+++ src/main/java/com/quick/recording/gateway/main/service/local/MainServiceAbstract.java
@@ -16,8 +16,11 @@ import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
@@ -53,111 +56,119 @@ public abstract class MainServiceAbstract<Entity extends SmartEntity,
     @CircuitBreaker(name = "database")
     public Dto byUuid(UUID uuid) {
         Assert.notNull(uuid, "Uuid cannot be null");
-        Entity entity = repository.findById(uuid).orElseThrow(
-                () -> new NotFoundException(messageUtil, entityClass, uuid)
+        Entity entity = getRepository().findById(uuid).orElseThrow(
+                () -> new NotFoundException(getMessageUtil(), getEntityClass(), uuid)
         );
-        return mapper.toDto(entity);
+        return getMapper().toDto(entity);
     }
 
     @Override
     @CircuitBreaker(name = "database")
-    public Page<Dto> search(Dto dto, Pageable pageable) {
-        Entity entity = mapper.toEntity(dto);
-        Page<Dto> page = repository.findAll(createSearch(entity), pageable).map(mapper::toDto);
+    public Page<Dto> list(Dto dto, Pageable pageable) {
+        Entity entity = getMapper().toEntity(dto);
+        Page<Dto> page = getRepository().findAll(createSearch(entity), pageable).map(getMapper()::toDto);
         return page;
     }
 
     @Override
     @CircuitBreaker(name = "database")
+    @Transactional
     public Dto post(Dto dto) {
-        Entity entity = mapper.toEntity(dto);
-        beforePost(entity);
-        entity = repository.save(entity);
-        afterPost(entity);
-        return mapper.toDto(entity);
+        Entity entity = getMapper().toEntity(dto);
+        this.beforePost(entity, dto);
+        entity = getRepository().saveAndFlush(entity);
+        this.afterPost(entity, dto);
+        return getMapper().toDto(entity);
     }
 
 
     @Override
     @CircuitBreaker(name = "database")
+    @Transactional
     public Dto patch(Dto dto) {
         Assert.notNull(dto.getUuid(), "Uuid cannot be null");
-        Entity entity = repository.findById(dto.getUuid()).orElseThrow(
-                () -> new NotFoundException(messageUtil, entityClass, dto.getUuid())
+        Entity entity = getRepository().findById(dto.getUuid()).orElseThrow(
+                () -> new NotFoundException(getMessageUtil(), getEntityClass(), dto.getUuid())
         );
-        beforePatch(entity);
-        setNoEditableField(dto, entity);
-        setPatchField(dto, entity);
-        entity = mapper.toEntityWithoutNull(dto, entity);
-        entity = repository.save(entity);
-        afterPatch(entity);
-        return mapper.toDto(entity);
+        this.beforePatch(entity, dto);
+        this.setNoEditableField(dto, entity);
+        this.setPatchField(dto, entity);
+        entity = getMapper().toEntityWithoutNull(dto, entity);
+        entity = getRepository().saveAndFlush(entity);
+        this.afterPatch(entity, dto);
+        return getMapper().toDto(entity);
     }
 
 
     @Override
     @CircuitBreaker(name = "database")
+    @Transactional
     public Dto put(Dto dto) {
         Assert.notNull(dto.getUuid(), "Uuid cannot be null");
-        Entity entity = repository.findById(dto.getUuid()).orElseThrow(
-                () -> new NotFoundException(messageUtil, entityClass, dto.getUuid())
+        Entity entity = getRepository().findById(dto.getUuid()).orElseThrow(
+                () -> new NotFoundException(getMessageUtil(), getEntityClass(), dto.getUuid())
         );
-        beforePut(entity);
-        setNoEditableField(dto, entity);
-        entity = mapper.toEntity(dto, entity);
-        entity = repository.save(entity);
-        afterPut(entity);
-        return mapper.toDto(repository.save(entity));
+        this.beforePut(entity, dto);
+        this.setNoEditableField(dto, entity);
+        entity = getMapper().toEntity(dto, entity);
+        entity = getRepository().saveAndFlush(entity);
+        this.afterPut(entity, dto);
+        return getMapper().toDto(entity);
     }
 
 
     @Override
     @CircuitBreaker(name = "database")
-    public Boolean delete(UUID uuid, Delete delete) {
+    @Transactional
+    public Dto delete(UUID uuid, Delete delete) {
         Assert.notNull(uuid, "Uuid cannot be null");
         return switch (delete) {
             case HARD -> {
-                repository.findById(uuid).ifPresent(repository::delete);
-                yield true;
+                Entity entity = getRepository().findById(uuid).orElseThrow(
+                        () -> new NotFoundException(getMessageUtil(), getEntityClass(), uuid)
+                );
+                this.getRepository().delete(entity);
+                yield this.getMapper().toDto(entity);
             }
             case SOFT -> {
-                Entity entity = repository.findById(uuid).orElseThrow(
-                        () -> new NotFoundException(messageUtil, entityClass, uuid)
+                Entity entity = getRepository().findById(uuid).orElseThrow(
+                        () -> new NotFoundException(getMessageUtil(), getEntityClass(), uuid)
                 );
                 entity.setIsActive(false);
-                yield !repository.save(entity).getIsActive();
+                yield this.getMapper().toDto(getRepository().save(entity));
             }
         };
     }
 
     @Override
     @CircuitBreaker(name = "database")
-    public Boolean restore(UUID uuid) {
+    @Transactional
+    public Dto restore(UUID uuid) {
         Assert.notNull(uuid, "Uuid cannot be null");
-        Entity entity = repository.findById(uuid).orElseThrow(
-                () -> new NotFoundException(messageUtil, entityClass, uuid)
+        Entity entity = getRepository().findById(uuid).orElseThrow(
+                () -> new NotFoundException(getMessageUtil(), getEntityClass(), uuid)
         );
         entity.setIsActive(true);
-        return repository.save(entity).getIsActive();
+        return this.getMapper().toDto(getRepository().save(entity));
     }
 
     @Override
     @CircuitBreaker(name = "database")
     public Entity save(Entity entity) {
-        return repository.save(entity);
+        return getRepository().save(entity);
     }
 
     @Override
     @CircuitBreaker(name = "database")
     public List<Entity> saveAll(Collection<Entity> list) {
-        return repository.saveAll(list);
+        return getRepository().saveAll(list);
     }
 
     @Override
     @CircuitBreaker(name = "database")
     public Entity byUuidEntity(UUID uuid) {
-        return repository.findById(uuid).orElseThrow(
-                () -> new NotFoundException(messageUtil, entityClass, uuid)
+        return getRepository().findById(uuid).orElseThrow(
+                () -> new NotFoundException(getMessageUtil(), getEntityClass(), uuid)
         );
     }
 
@@ -167,27 +178,33 @@ public abstract class MainServiceAbstract<Entity extends SmartEntity,
         return repository.findAll();
     }
 
-    protected void beforePost(Entity entity) {
+    @Transactional(propagation = Propagation.MANDATORY)
+    protected void beforePost(Entity entity, Dto dto) {
 
     }
 
-    protected void afterPost(Entity entity) {
+    @Transactional(propagation = Propagation.MANDATORY)
+    protected void afterPost(Entity entity, Dto dto) {
 
     }
 
-    protected void beforePatch(Entity entity) {
+    @Transactional(propagation = Propagation.MANDATORY)
+    protected void beforePatch(Entity entity, Dto dto) {
 
     }
 
-    protected void afterPatch(Entity entity) {
+    @Transactional(propagation = Propagation.MANDATORY)
+    protected void afterPatch(Entity entity, Dto dto) {
 
     }
 
-    protected void beforePut(Entity entity) {
+    @Transactional(propagation = Propagation.MANDATORY)
+    protected void beforePut(Entity entity, Dto dto) {
 
     }
 
-    protected void afterPut(Entity entity) {
+    @Transactional(propagation = Propagation.MANDATORY)
+    protected void afterPut(Entity entity, Dto dto) {
 
     }
 
@@ -197,14 +214,84 @@ public abstract class MainServiceAbstract<Entity extends SmartEntity,
                 .withIgnoreNullValues()
                 .withIgnoreCase()
                 .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
-        prepareExampleMatcher(exampleMatcher);
+        exampleMatcher = prepareExampleMatcher(exampleMatcher);
         return Example.of(entity, exampleMatcher);
     }
 
+    public abstract ExampleMatcher prepareExampleMatcher(ExampleMatcher exampleMatcher);
+
+    public Repository getRepository() {
+        return repository;
+    }
+
+    public Mapper getMapper() {
+        return mapper;
+    }
+
+    public MessageUtil getMessageUtil() {
+        return messageUtil;
+    }
+
+    public Class<Entity> getEntityClass() {
+        return entityClass;
+    }
+
+    private BaseDto clearValueDto(Object valueDto) throws NoSuchMethodException,
+            InvocationTargetException, InstantiationException, IllegalAccessException {
+        Constructor<?> constructor = valueDto.getClass().getConstructor();
+        BaseDto result = (BaseDto) constructor.newInstance();
+        result.setUuid(((BaseDto) valueDto).getUuid());
+        return result;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected void undoEditChild(Dto dto, Entity entity) {
+        Class<? extends SmartDto> dtoClass = dto.getClass();
+        List<Field> fieldsSingleDto = ReflectUtil.classTypeFieldFromClass(dtoClass, BaseDto.class);
+        List<Field> fieldsArray = ReflectUtil.arrayFieldFromClass(dtoClass);
+        ReflectUtil.DataWorkerList dataWorkerSingle = ReflectUtil.getDataWorker(fieldsSingleDto, dtoClass);
+        ReflectUtil.DataWorkerList dataWorkerArray = ReflectUtil.getDataWorker(fieldsArray, dtoClass);
+        try {
+            for (Field field : fieldsSingleDto) {
+                ReflectUtil.DataWorker dataWorker = dataWorkerSingle.getDataWorker(field.getName());
+                if (Objects.nonNull(dataWorker)) {
+                    Object valueDto = dataWorker.getGetter().invoke(dto);
+                    if (Objects.isNull(valueDto)) {
+                        continue;
+                    }
+                    if (BaseDto.class.isAssignableFrom(valueDto.getClass())) {
+                        dataWorker.getSetter().invoke(dto, clearValueDto(valueDto));
+                    }
+                }
+            }
+            try {
+                for (Field field : fieldsArray) {
+                    ReflectUtil.DataWorker dataWorker = dataWorkerArray.getDataWorker(field.getName());
+                    Collection<? super BaseDto> collection = (Collection<? super BaseDto>) dataWorker.getGetter().invoke(dto);
+                    if (Objects.isNull(collection)) {
+                        continue;
+                    }
+                    Collection<? super BaseDto> result = new ArrayList<>();
+                    for (Object baseDto : collection) {
+                        result.add(clearValueDto(baseDto));
+                    }
+                    dataWorker.getSetter().invoke(dto, result);
+                }
+            } catch (ClassCastException ignored) {
+            }
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException exception) {
+            log.error(
+                    String.format("Error in undoEditChild for class : %s \nerror: %s", dtoClass.getName(),
+                            exception.getMessage()));
+        }
+    }
+
+    @Transactional(propagation = Propagation.MANDATORY)
     protected void setNoEditableField(Dto dto, Entity entity) {
         dto.setIsActive(entity.getIsActive());
         dto.setCreatedBy(entity.getCreatedBy());
         dto.setCreatedWhen(entity.getCreatedWhen());
+        undoEditChild(dto, entity);
     }
 
     @SuppressWarnings("unchecked")
@@ -220,10 +307,16 @@ public abstract class MainServiceAbstract<Entity extends SmartEntity,
                 ReflectUtil.DataWorker dataWorkerDto = dataWorkerListDto.getDataWorker(field.getName());
                 if (Objects.nonNull(dataWorkerDto)) {
                     Object invokeList = dataWorkerDto.getGetter().invoke(dto);
+                    if (Objects.isNull(invokeList)) {
+                        invokeList = new ArrayList<>();
+                    }
                     if (Collection.class.isAssignableFrom(invokeList.getClass())) {
                         try {
                             Collection<? super BaseDto> oldCollection = (Collection<? super BaseDto>)
                                     dataWorkerDto.getGetter().invoke(oldDto);
+                            if (Objects.isNull(oldCollection)) {
+                                oldCollection = new ArrayList<>();
+                            }
                             Set<UUID> uuidSet = oldCollection.stream().map(item -> ((BaseDto) item).getUuid())
                                     .collect(Collectors.toSet());
                             Collection<? extends BaseDto> newCollection = (Collection<? extends BaseDto>) invokeList;
@@ -243,24 +336,6 @@ public abstract class MainServiceAbstract<Entity extends SmartEntity,
                     String.format("Error in setPatchField for class : %s \nerror: %s", dtoClass.getName(),
                             exception.getMessage()));
         }
-    }
-
-    public abstract ExampleMatcher prepareExampleMatcher(ExampleMatcher exampleMatcher);
-
-    public Repository getRepository() {
-        return repository;
-    }
-
-    public Mapper getMapper() {
-        return mapper;
-    }
-
-    public MessageUtil getMessageUtil() {
-        return messageUtil;
-    }
-
-    public Class<Entity> getEntityClass() {
-        return entityClass;
     }
 
 }
