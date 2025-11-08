7570af33b0a03b26f74e304362384181ecc1fb51
Fort163 <Front-91@mail.ru>
2025-11-08 16:57:15 +0400

    add simple user role name constant

--- src/main/java/com/quick/recording/gateway/Constant.java
+++ src/main/java/com/quick/recording/gateway/Constant.java
@@ -5,5 +5,6 @@ public class Constant {
     public static int PART_TIME_MINUTES = 30;
     public static final String LOCALE_COOKIE_NAME = "i18next";
     public static final String TRACK_ID_HEADER_NAME = "trackId";
+    public static final String ROLE_SIMPLE_USER = "SIMPLE_USER";
 
 }
